86e0a682e9e4125e7464289e49f18d30f97c7d2f
Rhianna Hepburn <rhianna@genvis.co>
2025-04-24 17:33:45 +0800

    Adds Sign Up page route to correctly navigate to Sign Up form

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template
 
 application = Flask(__name__)
+application.config['SECRET_KEY'] = 'your_secret_key_here' ##TODO: Change this to a random secret key
 
 import app.routes
+import app.forms
 
 if __name__ == '__main__':
     application.run(debug=True)
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,15 @@
 from app import application
 from flask import render_template
+from app.forms.sign_up_form import SignUpForm
 
 @application.route('/')
-def index():
-    return render_template('test.html')
+def home():
+    return render_template('home.html')
 
+@application.route('/signup')
+def signup():
+    form = SignUpForm()
+    if form.validate_on_submit():
+        # Handle sign-up logic here
+        pass
+    return render_template('sign_up_page.html', form=form)
