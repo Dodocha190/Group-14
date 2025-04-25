033207bdfc6957964e6d9b685811462826afa6a6
Dodocha190 <98612729+Dodocha190@users.noreply.github.com>
2025-04-25 14:45:32 +0800

    Update routes.py
    Changes:
    1. To include related module imports for login validation, guest login and registration validation
    
    2. Implementing blueprint to organize code
    
    3. Implementing the login route '/login' which:
     - creates a login session using login_manager
     - validates username and password from the database (works 
       with a login.html file under templates folder)
     - allows for guest login (needs to work with a login.html) by 
      using a booleanfield to indicate if the user wants to login as 
      guest and all their activity will be recorded under the 'guest'
      username in the user database (Will need a further commit for 
      login.html)
    
    4. Implementing the register route '/register' which:
     - Takes in username and password to be registered in the 
       user database
     - Works with register.html in templates folder to validate if 
       username is unique and password is valid/strong.
    
    5. Implementing the dashboard route '/dashboard' which:
       -  Currently has nothing, but is an example of a login required 
          access, will be used to display account profile after logging in
    
    6. Implementing the logout route '/logout' which:
       - Ends the login session using login manager
       - Redirects the user back to the login page

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,54 @@
-from app import application
-from flask import render_template
+from flask import Blueprint, render_template, redirect, url_for, flash
+from .forms import LoginForm, RegisterForm
+from .models import db, User
+from flask_login import login_user, logout_user, login_required, current_user
 
-@application.route('/')
-def index():
-    return render_template('test.html')
+main = Blueprint('main', __name__)
 
+#Login route
+@main.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.guest.data:
+            guest_user = User.query.filter_by(username='guest').first()
+            if not guest_user:
+                guest_user = User(username='guest')
+                guest_user.set_password('')
+                db.session.add(guest_user)
+                db.session.commit()
+            login_user(guest_user)
+            return redirect(url_for('main.dashboard'))
+        else:
+            user = User.query.filter_by(username=form.username.data).first()
+            if user and user.check_password(form.password.data):
+                login_user(user)
+                return redirect(url_for('main.dashboard'))
+            flash("Invalid username or password.")
+    return render_template('login.html', form=form)
+
+#Register route
+@main.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash("Registration successful. Please log in.")
+        return redirect(url_for('main.login'))
+    return render_template('register.html', form=form)
+
+#An example of how to implement certain sections that only logged in users can access (like an account dashboard)
+@main.route('/dashboard')
+@login_required
+def dashboard():
+    return f"Welcome, {current_user.username}!"
+
+@main.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash("You have been logged out.")
+    return redirect(url_for('main.login'))
