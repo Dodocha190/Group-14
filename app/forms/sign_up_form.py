b952c79ea9bcebc59497db99835f11eb5f2966fa
Dodocha190 <98612729+Dodocha190@users.noreply.github.com>
2025-04-25 15:41:11 +0800

    Update sign_up_form.py
    To validate if the same username has been registered previously

--- app/forms/sign_up_form.py
+++ app/forms/sign_up_form.py
@@ -8,3 +8,8 @@ class SignUpForm(FlaskForm):
     password = PasswordField('Create a password:', validators=[DataRequired()])
     study_field = StringField('Enter your study field:', validators=[DataRequired()])
     submit = SubmitField('Join for free')
+
+def validate_username(self, username):
+        existing_user = User.query.filter_by(username=username.data).first()
+        if existing_user:
+            raise ValidationError("Username already taken.")
