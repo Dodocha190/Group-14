7cee9393fe14cd53fc7fd27e4cad9b22241dc468
Dodocha190 <98612729+Dodocha190@users.noreply.github.com>
2025-04-25 19:06:55 +0800

    Update functions.js
    For toggleGuest, when users choose to view website as guest

--- app/static/js/functions.js
+++ app/static/js/functions.js
@@ -14,4 +14,12 @@ $(document).ready(function() {
     $submitButton.prop('disabled', true);
     $emailInput.on('input', checkLoginValidation);
     $passwordInput.on('input', checkLoginValidation);
-});
+}
+  function toggleGuest(checkbox) {
+    const username = document.getElementById("username");
+    const password = document.getElementById("password");
+    const isGuest = checkbox.checked;
+
+    username.disabled = isGuest;
+    password.disabled = isGuest;
+  });
