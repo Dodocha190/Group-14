5515913a2f2507e000bf764f9958f7b0b268f698
Dodocha190 <98612729+Dodocha190@users.noreply.github.com>
2025-04-27 17:34:35 +0800

    Update login_form.py
    Changed all username dependencies to email, assuming we only accept email for login, and changed the validators to validate an email instead of strings

--- app/forms/login_form.py
+++ app/forms/login_form.py
@@ -1,29 +1,23 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, EqualTo, ValidationError
-from .models import User
+from wtforms.validators import DataRequired, Email
 
 class LoginForm(FlaskForm):
-    username = StringField('Username')
+    email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password')
-    remember_me = BooleanField('Remember Me?')
     submit = SubmitField('Sign In')
     guest = BooleanField('Continue as Guest')
-    #Guest: All the reviews/search enquiry/sharing resulted will be displayed under guess and stored under guest in the database
-    #Anonymous: The user chooses to go anonymous after signing in, using options/settings or clicking on anonymous when submitting the review
-     
-    #Server based authentication, users can't bypass even if disabling JavaScript
+
     def validate(self, extra_validators=None):
-        #default validators (i.e. username length and password special characters, etc), there's nothing for now but will update if you guys want any validations to be fone on username and password filled in the login page
+        # default Flask-WTF validators (email format, required fields, etc.)
         rv = super().validate(extra_validators)
         if not rv:
             return False
         
-        #If not guest, username and password must be filled out, further configuration in login.html to handle when the user chooses to continue as guest
-
+        # If not logging in as guest, ensure email and password are filled
         if not self.guest.data:
-            if not self.username.data:
-                self.username.errors.append("Username is required.")
+            if not self.email.data:
+                self.email.errors.append("Email is required.")
                 return False
             if not self.password.data:
                 self.password.errors.append("Password is required.")
