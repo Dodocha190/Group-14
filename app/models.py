2495df8dfbaf2fe587953b6c4b6691634be35952
Dodocha190 <98612729+Dodocha190@users.noreply.github.com>
2025-04-27 17:30:06 +0800

    Update models.py
    Changed primary key from user id to email and removed user id

--- app/models.py
+++ app/models.py
@@ -6,8 +6,7 @@ from flask_login import UserMixin
 db = SQLAlchemy()
 
 class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True) #Or you could use username here
-    email = db.Column(db.String(64), unique=True, nullable=False)
+    email = db.Column(db.String(64), unique=True, nullable=False, primary_key = True)
     password_hash = db.Column(db.String(128), nullable=False)
 
     def set_password(self, password):
