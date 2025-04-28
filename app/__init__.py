6d8fd25ee1b9f4d94456f0985eff7721a29a5314
Rhianna Hepburn <rhianna@genvis.co>
2025-04-28 08:24:15 +0800

    fixes import errors and routing errors

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from .config import Config
 from .models import db
-from .routes import register_routes
+
 
 application = Flask(__name__)
 application.config.from_object(Config)
 
 db.init_app(application)
-register_routes(application)
+
+import app.routes
+# register_routes(application)
