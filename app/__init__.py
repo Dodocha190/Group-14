1ea7269d189bd23666236d599bea619a4d266734
LAPTOP-6RMNSMV6\Katherine <kaorutakehikoo190@gmail.com>
2025-05-07 18:36:39 +0800

    Revert "Signup form and login form fixes"
    This reverts commit f26577574fc24a808dde1484f917f57a83e646ed.
    
    Reverted commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,31 +1,13 @@
 from flask import Flask
 from .config import Config
-from .models import db, User
+from .models import db
 from flask_migrate import Migrate
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
 
 
 application = Flask(__name__)
 application.config.from_object(Config)
 
 db.init_app(application)
-login_manager = LoginManager()
-login_manager.login_view = 'login'
-login_manager.init_app(application)
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
-
 migrate = Migrate(application, db)
 
 import app.routes
-
-with application.app_context():
-    db.create_all()
-
-@application.context_processor
-def inject_user():
-    from flask_login import current_user
-    return dict(show_user_info=current_user.is_authenticated)
--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,10 @@
 from app import application
-from flask import render_template, redirect, url_for, flash, request 
+from flask import render_template, redirect, url_for, flash
 from app.forms.login_form import LoginForm
 from app.forms.sign_up_form import SignUpForm
 from app.forms.unit_review import addUnitForm
 from app.forms.unit_review import reviewForm
 from .models import db, User
-from werkzeug.security import generate_password_hash
-from flask_login import login_user, current_user, logout_user, login_required
 
 @application.route('/')
 def home():
@@ -17,19 +15,15 @@ def unit_summary():
     return render_template('unit_summary.html')
 
 
-@application.route('/userhome') #temporary, somewhere to go to after successful login
-def userhome():
-        return render_template('userhome.html', show_user_info=True, user_email=current_user.username)
+@application.route('/dashboard') #temporary, somewhere to go to after successful login
+def dashboard():
+        return render_template('userhome.html', show_user_info=True, user_email='current_user.email')
 
 @application.route('/signup', methods=['GET', 'POST'])
 def signup():
     form = SignUpForm()
     if form.validate_on_submit():
-        user = User(
-            email=form.email.data,
-            username=form.username.data,
-            password_hash=generate_password_hash(form.password.data),
-            study_field=form.study_field.data)
+        user = User(email=form.email.data)
         user.set_password(form.password.data)
         db.session.add(user)
         db.session.commit()
@@ -41,20 +35,23 @@ def signup():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
-        if user and user.check_password(form.password.data):
-            login_user(user)
-            return redirect(url_for('userhome'))
-        flash("Invalid email or password.")
+        if form.guest.data:
+            guest = User.query.filter_by(email='guest@classmate.com').first()
+            if not guest:
+                guest = User(email='guest@classmate.com')
+                guest.set_password('')
+                db.session.add(guest)
+                db.session.commit()
+                return redirect(url_for('userhome')) #dashboard for now, will decide on it later
+        else:
+            user = User.query.filter_by(email=form.email.data).first()
+            if user and user.check_password(form.password.data):
+                return redirect(url_for('userhome'))
+            flash("Invalid email or password.")
 
     return render_template('login_page.html', form=form)
 
-@application.route("/logout")
-@login_required
-def logout():
-    logout_user()
-    return redirect(url_for("login"))  
-
+  
 @application.route('/search')
 def search():
     return render_template('unit_search.html')
