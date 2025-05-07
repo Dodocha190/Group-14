b33aab9997793331de712082fea77e0ca5cab8ff
Rhianna Hepburn <rhianna@genvis.co>
2025-05-07 15:32:18 +0800

    creates all tables in database on init

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,8 @@ application = Flask(__name__)
 application.config.from_object(Config)
 
 db.init_app(application)
+with application.app_context():
+    db.create_all()
 migrate = Migrate(application, db)
 
 import app.routes
