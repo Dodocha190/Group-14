d382e1547df26aa349ff8fb0c22b19739b34e751
Rhianna Hepburn <rhianna@genvis.co>
2025-05-08 16:54:11 +0800

    changes foreign key references from email to id

--- app/models.py
+++ app/models.py
@@ -15,7 +15,7 @@ class User(UserMixin, db.Model):
 
 
     def set_password(self, password):
-        self.password_hash = generate_password_hash(password, method='pbkdf2:sha256')
+        self.password_hash = generate_password_hash(password, method='pbkdf2')
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
@@ -47,7 +47,7 @@ class Unit(db.Model):
 class DiaryEntry(db.Model):
     __tablename__ = 'diary_entries'
     id                       = db.Column(db.Integer, primary_key=True)
-    user_email               = db.Column(db.String(64), db.ForeignKey('users.email'), nullable=False)
+    user_id               = db.Column(db.String(64), db.ForeignKey('users.id'), nullable=False)
     unit_id                  = db.Column(db.Integer, db.ForeignKey('units.id'), nullable=False)
     semester                 = db.Column(db.Integer, nullable=False)
     year                     = db.Column(db.Integer, nullable=False)
@@ -57,7 +57,7 @@ class DiaryEntry(db.Model):
     coordinator_rating       = db.Column(db.Integer)
     workload_hours_per_week  = db.Column(db.Integer)
     __table_args__ = (
-        db.UniqueConstraint('user_email','unit_id','semester', name='uix_user_unit_sem'),
+        db.UniqueConstraint('user_id','unit_id','semester', name='uix_user_unit_sem'),
     )
 
 
@@ -72,5 +72,5 @@ class AssessmentBreakdown(db.Model):
 class DiaryShare(db.Model):
     __tablename__ = 'diary_shares'
     id                = db.Column(db.Integer, primary_key=True)
-    owner_email       = db.Column(db.String(64), db.ForeignKey('users.email'), nullable=False)
-    recipient_email   = db.Column(db.String(64), db.ForeignKey('users.email'), nullable=False)
+    owner_id      = db.Column(db.String(64), db.ForeignKey('users.id'), nullable=False)
+    recipient_id   = db.Column(db.String(64), db.ForeignKey('users.id'), nullable=False)
