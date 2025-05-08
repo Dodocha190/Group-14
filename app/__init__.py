eee3d12a18b36b6a7122e0be8fd113180792e647
Rhianna Hepburn <rhianna@genvis.co>
2025-05-08 18:19:26 +0800

    correctly initialises login manager

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from .config import Config
-from .models import db
+from .models import db, User
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 
 application = Flask(__name__)
@@ -9,6 +10,8 @@ application.config.from_object(Config)
 
 db.init_app(application)
 migrate = Migrate(application, db)
+login_manager = LoginManager()
+login_manager.init_app(application)
 
 import app.routes
 
@@ -16,3 +19,7 @@ import app.routes
 def inject_user():
     from flask_login import current_user
     return dict(show_user_info=current_user.is_authenticated)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
