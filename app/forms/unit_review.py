f49b50a7ac9021162346e08c2fe7f554c14abaff
LAPTOP-6RMNSMV6\Katherine <kaorutakehikoo190@gmail.com>
2025-05-15 02:33:56 +0800

    Fixing errors caused by merging conflicts

--- app/forms/unit_review.py
+++ app/forms/unit_review.py
@@ -1,12 +1,7 @@
 from flask_wtf import FlaskForm
-<<<<<<< HEAD
-from wtforms import StringField, SubmitField, RadioField, IntegerField, SelectMultipleField, FloatField, TextAreaField
-from wtforms.validators import DataRequired, NumberRange
-=======
 from wtforms import StringField, SubmitField, RadioField, IntegerField, SelectMultipleField, FloatField, FieldList, FormField, SelectField, BooleanField
 from wtforms.validators import DataRequired, NumberRange, Optional
 from app.models import AssessmentType
->>>>>>> origin/main
 
 
 class AddUnitForm(FlaskForm):
@@ -39,37 +34,3 @@ def create_review_form():
             field_name = f"assessment_{assessment.name.lower().replace(' ', '_')}"
             field_label = f"{assessment.name}"
             locals()[field_name] = BooleanField(field_label, default=False) 
-
-<<<<<<< HEAD
-class ReviewForm(FlaskForm):
-    rev_uni = StringField("University:", validators=[DataRequired()])
-    rev_semester = IntegerField("Semester:", validators=[DataRequired()])
-    rev_code = StringField("Unit code:", validators=[DataRequired()])
-    rev_year = IntegerField("Year taken:", validators=[DataRequired(), NumberRange(min=1900, max=2050, message="Please enter a valid year")])
-    rev_unit_coord_rating = RadioField("Rate the unit coordinator from 1 to 5 (1 being poor, 5 being amazing)",
-                             choices=[('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5')], validators=[DataRequired()])
-    rev_difficulty = RadioField("Rate the difficulty of the unit from 1 to 5 (1 being easy, 5 being difficult)",
-                                 choices=[('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5')], validators=[DataRequired()])
-    rev_hours = IntegerField("Hours of contact per week:", validators=[DataRequired()])
-    rev_assessments = SelectMultipleField("Select all graded assessments",
-                                         choices=[('final', 'Final exam'), ('midsem', 'Midsemester exam'), ('project', 'Individual project'), ('group', 'Group project'), ('essay', 'Essay'), ('test', 'Test/quiz'), ('lab', 'Labs'), ('participation', 'Participation/attendance'), ('other', 'Other')], validators=[DataRequired()])
-    # need to make subform for user to enter weighting of assessments
-    rev_grade = FloatField("Your grade:", validators=[DataRequired(), NumberRange(min=0, max=100)])
-    rev_avg_hours = IntegerField("Average hours of study per week:", validators=[DataRequired()])
-    rev_rating = RadioField("Rate the unit overall",
-                            choices=[('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5')], validators=[DataRequired()])
-    rev_comments = TextAreaField("Additional Comments (optional):")
-    rev_submit = SubmitField("Submit")
-=======
-        def get_selected_assessments(self):
-            selected_assessments = []
-            for field_name, field in self._fields.items():
-                print(f"Field name: {field_name}, Field data: {field.data}")
-                if field_name.startswith("assessment_") and field.data == True:
-                    assessment_name = field_name.replace("assessment_", "").replace("_", " ").title()
-                    selected_assessments.append(assessment_name)
-                    print(f"Selected assessment: {assessment_name}")
-            return selected_assessments
-
-    return ReviewForm()
->>>>>>> origin/main
